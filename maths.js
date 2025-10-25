a54540772078aca4a116b4903a2b5c88fd553a48
harman gulati <harman.gulati1989@gmail.com>
2025-10-25 17:12:19 +0530

    First Commit

---
+++ maths.js
@@ -0,0 +1,19 @@
+const add= (a,b) => {
+    return a + b;
+}
+
+const sub= (a,b) => {
+    return a - b;
+}
+
+const div= (a,b) => {
+    return a / b;
+}
+
+const multiple= (a,b) => {
+    return a * b;
+}
+
+
+
+module.exports = { add, sub, div, multiple };
